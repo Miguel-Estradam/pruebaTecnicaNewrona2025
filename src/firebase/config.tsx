73f19daaf37b9d7b096dfda95f8bddd86cb6e204
Miguel Alejandro Munoz <miguelestradam12@gmail.com>
2025-05-03 10:55:35 -0500

    create project

---
+++ src/firebase/config.tsx
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCvcimYn2TRJ-0UuwL1drZgRsB5UAzrq_c",
+  authDomain: "pruebatecnicanewrona-22ae5.firebaseapp.com",
+  projectId: "pruebatecnicanewrona-22ae5",
+  storageBucket: "pruebatecnicanewrona-22ae5.firebasestorage.app",
+  messagingSenderId: "707275461823",
+  appId: "1:707275461823:web:a2b10dd121d38029af220a",
+  measurementId: "G-0MX06SJLPB",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
